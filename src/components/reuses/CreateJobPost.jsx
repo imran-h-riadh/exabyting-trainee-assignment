ff521628e8be9958f631f37a1070741f895da892
imran hossain riadh <riadh@imrans-MacBook-Air.local>
2024-06-25 18:37:31 +0600

    master created

--- src/components/reuses/CreateJobPost.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function CreateJobPost() {
-  return (
-    <div>CreateJobPost</div>
-  )
-}
--- src/components/reuses/JobDetails.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function JobDetails() {
-  return (
-    <div>JobDetails</div>
-  )
-}
